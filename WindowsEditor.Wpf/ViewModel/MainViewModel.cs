c93554e0dcf0c254a06017922cefb47dcf93097b
Anton Sizikov <sizikov.anton@gmail.com>
2014-06-26 04:20:07 -0700

    basic logic: load image to imagebox

--- WindowsEditor.Wpf/ViewModel/MainViewModel.cs
+++ WindowsEditor.Wpf/ViewModel/MainViewModel.cs
@@ -1,57 +1,87 @@
-﻿using System.Globalization;
+﻿using System.Drawing;
 using System.Windows.Input;
+using CrossPlatformLogic;
+using Microsoft.Win32;
 
 namespace WindowsEditor.Wpf.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        private string _title;
-        private string _tbText;
-        private ICommand _buttonCommand;
-        private int counter;
+        private ICommand buttonCommand;
+        private ImageLoader imageLoader;
+        private string tbText;
+        private string title;
+        private Image image;
 
         public MainViewModel()
         {
             Title = "Our nice windows editor";
-            ButtonCommand = new RelayCommand(_ => OnClick()); 
-        }
-
-        private void OnClick()
-        {
-            counter++;
-            TbText = "hop " + counter.ToString(CultureInfo.InvariantCulture);
+            imageLoader = new ImageLoader();
+            ButtonCommand = new RelayCommand(_ => OnClick());
         }
 
 
         public string Title
         {
-            get { return _title; }
+            get { return title; }
             set
             {
-                if (value == _title) return;
-                _title = value;
+                if (value == title) return;
+                title = value;
                 OnPropertyChanged();
             }
         }
 
         public ICommand ButtonCommand
         {
-            get { return _buttonCommand; }
+            get { return buttonCommand; }
             set
             {
-                if (Equals(value, _buttonCommand)) return;
-                _buttonCommand = value;
+                if (Equals(value, buttonCommand)) return;
+                buttonCommand = value;
                 OnPropertyChanged();
             }
         }
 
-        public string TbText
+        public string FilePath
         {
-            get { return _tbText; }
+            get { return tbText; }
             set
             {
-                if (value == _tbText) return;
-                _tbText = value;
+                if (value == tbText) return;
+                tbText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnClick()
+        {
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".jpg",
+                Filter = "Image files (*.jpg, *.jpeg, *.png) | *.jpg; *.jpeg; *.png"
+            };
+            var result = dlg.ShowDialog();
+
+            if (result != true) return;
+
+
+            string filename = dlg.FileName;
+            FilePath = filename;
+            var img = imageLoader.LoadImage(filename);
+            if (img != null)
+            {
+                Image = img;
+            }
+        }
+
+        public Image Image
+        {
+            get { return image; }
+            set
+            {
+                if (Equals(value, image)) return;
+                image = value;
                 OnPropertyChanged();
             }
         }
